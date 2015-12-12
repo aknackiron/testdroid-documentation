f3e69e33c15395df81d9839627e1c2432bbac4c3
Toni Vaakanainen <toni.vaakanainen@bitbar.com>
2015-12-12 16:33:53 +0200

    CSS split, and navigation updating

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    /* Open navigation element if sub element is active */
+    activeNavigationElement = $("section .navigation .navigation-closed .active");
+    if (activeNavigationElement.length == 1){
+        parents = $(activeNavigationElement).parents(".navigation-closed");
+        parents.removeClass("navigation-closed").addClass("navigation-open");
+    }
+    
+    $("section .navigation .navigation-icon").click(function(){
+        parent = $(this).parent("li");
+        if (parent.hasClass("navigation-closed")){
+            parent.removeClass("navigation-closed").addClass("navigation-open");
+        }
+        else if (parent.hasClass("navigation-open")){
+            parent.addClass("navigation-closed").removeClass("navigation-open");
+        }
+    }); 
+});
