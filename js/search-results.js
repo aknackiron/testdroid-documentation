53a7137d8327e582ba84722d43c12e376b6a751f
Toni Vaakanainen <toni.vaakanainen@bitbar.com>
2016-01-06 16:32:08 +0200

    Search js cleanup part 1

--- js/search-results.js
+++ js/search-results.js
@@ -6,7 +6,6 @@
  * 4. Implement cookie based data save as a backup
  * */
 
-
 //Modification of http://frontendcollisionblog.com/javascript/jekyll/tutorial/2015/03/26/getting-started-with-a-search-engine-for-your-site-no-server-required.html by Josh Beam
 $(document).ready(function(){
 
@@ -18,12 +17,9 @@ $(document).ready(function(){
 		   
 		query.setFromURL('query');
 		vars = {query: query, site: site, params: params};
+		
 		/* Check if specific query has been made previously */
 		var readResult = readSearchResults(query.q);
-		
-		/* DEBUGGING, REMOVE */
-		
-		/* DEBUGGING, REMOVE */
 		
 		if (!readResult){
 			/* NOTICE: getJSON url needs to be fixed */   
@@ -63,25 +59,10 @@ $(document).ready(function(){
 		}
 		
     }(Query));
-    
-    /* TODO:
-     * 1. Separate search possible results get and processiing
-     * 2. Defer results creation into web worker, it is currently the heaviest step and freezes the thread for ~1,3s
-     * 3. Push results to browser cookie in case of page change or new query with the same term
-     * 4. Modify script to fetch results from cookie if such exists
-     *   */
-    
 });
 
 function handleResults(vars){
 	/* Use search-worker.js also without worker available, use just as a normal script */
-	/* DEBUGGIN, REMOVE */
-	workerFallback(vars);
-	/*$(document).ready(function(){
-		launchOutsideWorker(vars);
-	});*/
-	return;
-	/*DEBUGGING, REMOVE*/
 	
 	if(typeof(Worker) !== "undefined") {
 		// call worker function from search index, easier to have it there to modify script url by liquid
@@ -89,182 +70,13 @@ function handleResults(vars){
 		w.postMessage(vars);
 	}
 	else{
-		processResults(vars);
+		workerFallback(vars);
 	}
 }
 
-// http://stackoverflow.com/questions/8586446/dynamically-load-external-javascript-file-and-wait-for-it-to-load-without-usi thanks to Jason Sebring
-//function getScript(src, callback, vars) {
-function getScript(src, callback) {
-	var s = document.createElement('script');
-	s.src = src;
-	s.async = true;
-	s.onreadystatechange = s.onload = function() {
-		if (!callback.done && (!s.readyState || /loaded|complete/.test(s.readyState))) {
-			callback.done = true;
-//			callback(vars);
-			callback();
-		}
-	};
-	document.querySelector('head').appendChild(s);
-}
-
-function processResults(vars){
-    var data = vars.data,
-        query = vars.query,
-        site = vars.site,
-        params = vars.params,
-        contentMaxLength = 400, // Max lenght of content snippet
-		visibleResults = 10; // Results per page
-
-    var searchIndex,
-            results,
-            $results = $('.search-results'),
-            totalScore = 0,
-            percentOfTotal;
-
-    // set up the allowable fields
-    searchIndex = lunr(function() {
-            this.field('title');
-            //this.field('category');
-            this.field('content');
-            this.ref('url');
-            //this.field('date');
-    });
-
-    /* THIS IS THE HEAVIES STEP */
-    // add each item from posts.json to the index
-    $.each(data,function(i,item) {
-            searchIndex.add(item);
-    });
-; 
-    // search for the query and store the results as an array
-    results = searchIndex.search(query.get());
-
-    // add the title of each post into each result, too (this doesn't come standard with lunr.js)
-    for(var result in results) {                      
-        for(var dataIndex in data){
-            if (results[result].ref === data[dataIndex].url){
-                results[result].title = data[dataIndex].title;
-                results[result].content = data[dataIndex].content.substr(0, contentMaxLength);
-            }
-        }
-    }
-   
-    // get the total score of all items, so that we can divide each result into it, giving us a percentage
-    $.each(results, function(i, result) {
-            totalScore+=result.score;
-    });
-
-    var vars = params.split("&");
-
-    var pageParam = {},
-		queryParam = {},
-		queryWords = [];
-    for (var i=0;i<vars.length;i++) {
-        var helper = vars[i].split("=");
-        if (helper[0] == "page"){
-            pageParam[helper[0]] = parseInt(helper[1]);
-        }
-        if (helper[0] == "query"){
-            queryParam[helper[0]] = helper[1];
-            queryWords = decodeURI(helper[1]).split(" ");
-        }
-    }
-
-    var startingIndex = 1;
-    if (pageParam.page && !isNaN(pageParam.page)){                    
-        startingIndex = pageParam.page;
-    }
-    startingIndex = (startingIndex - 1) * visibleResults;
-    var endingIndex = startingIndex * visibleResults + visibleResults;
-
-    var processKeywords = function(contentString){
-        // Thanks to Ryan O'Hara http://stackoverflow.com/questions/17264639/replace-text-but-keep-case
-        function matchCase(text, pattern) {
-            var result = '';
-
-            for(var i = 0; i < text.length; i++) {
-                var c = text.charAt(i);
-                var p = pattern.charCodeAt(i);
-
-                if(p >= 65 && p < 65 + 26) {
-                    result += c.toUpperCase();
-                } else {
-                    result += c.toLowerCase();
-                }
-            }
-
-            return result;
-        }        
-
-        // queryWords
-        for (var index in queryWords){
-            r = new RegExp( "(" + queryWords[index] + ")" , 'gi' );
-            contentString = contentString.replace(r, function(match) {
-                return "<b>"+matchCase(queryWords[index], match)+"</b>";
-            });
-        }
-
-        // html decode result content
-        // Thanks to lucascaro & Mark Amery http://stackoverflow.com/questions/1147359/how-to-decode-html-entities-using-jquery
-        function decodeEntities(encodedString) {
-            var textArea = document.createElement('textarea');
-            textArea.innerHTML = encodedString;
-            return textArea.value;
-        }
-
-        contentString = decodeEntities(contentString);
-
-        return contentString;
-    };
-
-    for (var i = startingIndex; i <= endingIndex; i++){
-        var result = results[i];
-
-        if (i >= results.length){
-            break;
-        }
-
-        if (i < endingIndex){
-
-            percentOfTotal = result.score/totalScore;
-
-            //Process keywords
-            if (queryWords.length){
-                result.content = processKeywords(result.content);
-            }
-
-            $results.append('<li class="search-result"><a href="'+ site + result.ref +'">'+result.title+'</a><p>'+decodeURI(result.content)+'</p></li>');
-        }
-    }
-    var amountOfPages = Math.ceil(results.length / visibleResults);
-
-    if (amountOfPages > 1){
-        pages = function(){
-            element = "";
-            for (var j = 1; j <= amountOfPages; j++){
-                if (element.length){
-                    element += " / ";
-                }
-                if (j == pageParam.page || (j == 1 && typeof pageParam.page === "undefined") ){
-                    element += "<span>"+j+"</span>";
-                }
-                else{
-                    element += "<a href='"+baseURL+"/search?query="+queryParam.query+"&page="+j+"'>"+j+"</a>";
-                }
-            }
-            return element;
-        }
-
-        $results.append(pages());
-    }
- 
-    $(".content .title").append(' for "'+decodeURI(queryParam.query)+'"');
-    if (!results.length){
-        $results.append('<li class="search-result"><p>No results for "'+decodeURI(queryParam.query)+'".</p></li>');
-    }
-}
+/* Handlers for communication with search-worker script
+ * search/index.md functions as an intermediate because of useful liquid 
+ * funtionalities of getting access to base url */
 
 function notWorkerPostMessage(data){
 	handleWorkerMessage(data);
@@ -283,65 +95,9 @@ function handleWorkerMessage(data){
 	}
 }
 
-function writeSearchResults(data){
-	if (typeof(Storage) !== "undefined"){
-		writeResultsToSessionStorage(data);
-	}
-	else{
-		writeResultsToCookie(data);
-	}
-}
-
-function readSearchResults(query){
-	console.log("readSearchResults fired!");
-	console.log(query);
-
-	if (typeof(Storage) !== "undefined"){
-		readResult = readResultsFromSessionStorage(query);
-		return readResult;
-	}
-	return false;
-}
-
-function writeResultsToSessionStorage(data){
-	console.log("writeResultsToSessionStorage");
-	console.log(data);
-	
-	console.log(data.query);
-	console.log(JSON.stringify(data.results));
-	
-	sessionStorage.setItem(data.query, JSON.stringify(data.results));
-	
-	//Test functionality
-	//readResultsFromSessionStorage();
-}
-
-function writeResultsToCookie(data){
-	console.log("writeResultsToCookie");
-	console.log(data);
-	
-	
-}
-
-function readResultsFromSessionStorage(query){
-	console.log("readResultsFromSessionStorage");
-	console.log(query);
-	
-	var data = JSON.parse(sessionStorage.getItem(query));
-	
-	console.log("this!!!!!!!!!! data");
-	console.log(data);
-	
-	if (data != null){
-		return data;
-	}
-	return false;
-}
-
-function readResultsFromCookie(){
-	
-}
-
+/* DOM functionalities with existing results as Web Workers have no
+ * access to DOM */
+ 
 function displayWorkerResults(data){
 	function decodeEntities(encodedString) {
         var textArea = document.createElement('textarea');
@@ -405,4 +161,48 @@ function displayWorkerResults(data){
             }
         }   
     }  
+}
+
+/* Write results functionalities:
+ * Use session storage as it has been default functionality of browsers for long.
+ * No point to see the extra effort to create a cookie fallback */
+ 
+function writeSearchResults(data){
+	if (typeof(Storage) !== "undefined"){
+		sessionStorage.setItem(data.query, JSON.stringify(data.results));
+	}
+}
+
+/* Read results functionalities:
+ * Use session storage as it has been default functionality of browsers for long.
+ * No point to see the extra effort to create a cookie fallback */
+
+function readSearchResults(query){
+	console.log("readSearchResults fired!");
+	console.log(query);
+
+	if (typeof(Storage) !== "undefined"){
+		var data = JSON.parse(sessionStorage.getItem(query));
+		
+		if (data != null){
+			return data;
+		}
+	}
+	return false;
+}
+
+/* Function to import search-worker script incase Web Worker is not supported */
+
+// http://stackoverflow.com/questions/8586446/dynamically-load-external-javascript-file-and-wait-for-it-to-load-without-usi thanks to Jason Sebring
+function getScript(src, callback) {
+	var s = document.createElement('script');
+	s.src = src;
+	s.async = true;
+	s.onreadystatechange = s.onload = function() {
+		if (!callback.done && (!s.readyState || /loaded|complete/.test(s.readyState))) {
+			callback.done = true;
+			callback();
+		}
+	};
+	document.querySelector('head').appendChild(s);
 }
