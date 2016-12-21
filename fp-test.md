692e48d4f7b5b2a1b933441f7b1139417a9613ad
Tommi Martin <tommi.martin@bitbar.com>
2016-12-21 10:41:58 +0200

    New front page template test.

---
+++ fp-test.md
@@ -0,0 +1,22 @@
+---
+layout: frontpage
+title: frontpage-testing
+---
+
+
+# Welcome to Testdroid Cloud Online Documentation!
+
+- In addition to the documentation on these pages, check [our available](https://github.com/bitbar/testdroid-samples/) code on	Github. There are samples, code snippets and guides.
+	- &#xf113;
+- If you have any feedback on how to improve these guides, please leave	us a note at <feedback@bitbar.com> so we can improve these.
+	- &#xf003;
+- Support forum related to technical questions is available [here](http://stackoverflow.com/questions/tagged/testdroid) on Stackoverflow.
+	- &#xf16c;
+- For any technical feedback get in touch through <support@bitbar.com>.
+	- &#xf003;
+
+<div class="fp-para-frame" style="background-image: url('/assets/testdroid-blur.jpg'); background-size: cover;">
+<div>
+	<p>Looking for more information about <a href="http://bitbar.com/testing/">mobile app testing</a> with real Android and iOS devices? Bitbar Testing and Testdroid technology got you covered.</p>
+</div>
+</div>
--- js/main.js
+++ js/main.js
@@ -96,6 +96,12 @@ $(document).ready(function(){
     }); 
     
     var $resizableNavigation = $( "#resizable-navigation" );
+    var $altNav = false;
+    if ($resizableNavigation.length === 0) {
+      $resizableNavigation = $( "#resizable-navigation-right" );
+      $altNav = true;
+    }
+
     var windowWidth = $( window ).width();
     
     var navigationMaxWidth = windowWidth < 370 ? $( window ).width() - 20 : 350;
@@ -112,12 +118,23 @@ $(document).ready(function(){
 		$(ui.element[0]).removeClass('resizing');
 	}
     
-    $resizableNavigation.resizable({
+    if ($altNav) {
+      $resizableNavigation.resizable({
+        handles: 'w',
+        maxWidth: navigationMaxWidth,
+        start: resizing,
+        stop: stoppedResizing
+      });
+    }
+    else {
+      $resizableNavigation.resizable({
         handles: 'e',
         maxWidth: navigationMaxWidth,
         start: resizing,
         stop: stoppedResizing
-    });
+      });
+    }
+    
     
     // hide resize icon if not touch device
     if (!Modernizr.touch){
